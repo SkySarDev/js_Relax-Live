1b8e3bc8a03f7b1e9192afad9a54488a7384b754
SkySar <skysar@ya.ru>
2021-07-13 11:55:08 +0300

    Feat: add animation for .header-contacts__arrow

--- src/js/modules/showPhoneNumber.js
+++ src/js/modules/showPhoneNumber.js
@@ -1,9 +1,11 @@
 const showPhoneNumber = () => {
     const phoneNumberAccord = document.querySelector('.header-contacts__phone-number-accord'),
-        linkTel = phoneNumberAccord.querySelector('a');
+        linkTel = phoneNumberAccord.querySelector('a'),
+        headerContactsArrow = document.querySelector('.header-contacts__arrow img');
 
     phoneNumberAccord.classList.toggle('show-number');
     linkTel.classList.toggle('show-number');
+    headerContactsArrow.classList.toggle('show-number');
 };
 
 export default showPhoneNumber;
