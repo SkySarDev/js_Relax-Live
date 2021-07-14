26a1410abbbb26e07827307731a3afe474e58322
SkySar <skysar@ya.ru>
2021-07-14 11:15:15 +0300

    Feat: add phoneMask

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
+'use strict';
+
 import '../css/style.css';
 import '../index.html';
 
 import handlers from './modules/handlers';
+import phoneMask from './modules/phoneMask';
 
 handlers();
+phoneMask('[name="phone"]');
