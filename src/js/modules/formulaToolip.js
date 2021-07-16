759d564468cd3aa050514339a037b0145be2bfc9
SkySar <skysar@ya.ru>
2021-07-16 17:11:38 +0300

    Refactor: formulaTooltip

--- src/js/modules/formulaToolip.js
+++ src/js/modules/formulaToolip.js
@@ -1,11 +1,21 @@
-const formulaToolip = (element, show) => {
-    if (show) {
-        element.classList.add('active-item');
+const formulaToolip = ({ element, elementsHide, show }) => {
+    if (elementsHide)
+        document.querySelectorAll(elementsHide).forEach(element => {
+            element.classList.remove('active-item');
+        });
 
-        if (element.querySelector('.formula-item-popup').getBoundingClientRect().top < 0)
-            element.classList.add('rotate');
-    } else {
-        element.classList.remove('active-item', 'rotate');
+    if (element) {
+        if (show) {
+            element.classList.add('active-item');
+
+            if (element.querySelector('.formula-item-popup').getBoundingClientRect().top < 0) {
+                element.closest('.row').style.zIndex = 10;
+                element.classList.add('rotate');
+            }
+        } else {
+            element.classList.remove('active-item', 'rotate');
+            element.closest('.row').style.zIndex = '';
+        }
     }
 };
 
