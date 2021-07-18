5080f484c2e07b17f85e952244d33d5ada80e1e8
SkySar <skysar@ya.ru>
2021-07-18 12:33:41 +0300

    Chore: change redirect url

--- src/js/admin.js
+++ src/js/admin.js
@@ -7,7 +7,7 @@ import { getCookie, redirect } from './modules/admin/services';
 import listServices from './modules/admin/listServices';
 
 const API_URL = 'https://obscure-scrubland-21489.herokuapp.com';
-const currentUrl = window.location.origin;
+const currentUrl = window.location.origin + '/relax-life';
 
 if (!getCookie('demo')) redirect(currentUrl + '/admin');
 
--- src/js/login.js
+++ src/js/login.js
@@ -6,7 +6,7 @@ import '../css/admin.css';
 import auth from './modules/admin/auth';
 import { getCookie, redirect } from './modules/admin/services';
 
-const currentUrl = window.location.origin;
+const currentUrl = window.location.origin + '/relax-life';
 
 if (getCookie('demo')) redirect(currentUrl + '/admin/table.html');
 else auth(currentUrl);
